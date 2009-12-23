7ab25c5858a3ec3bc31cbdddb15e34ea3b951531
Justin DeWind <dewind@atomicobject.com>
2009-12-22 20:45:34 -0500

    replaced manual require statements with Bundler.require_env :test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,7 @@
 here = File.expand_path(File.dirname(__FILE__))
 require File.join(here, "..", "vendor/gems/environment")
 require File.join(here, "..", "init")
-
-require 'spec/autorun'
-require 'mocha'
-require 'active_support'
-require 'rack'
+Bundler.require_env :test
 
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
 
